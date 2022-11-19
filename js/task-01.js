c395322554d66e563fca1e48c8c382a7f84bd182
Vladyslav Belotskiy <vlad.belotskiy9595@gmail.com>
2022-11-19 10:53:54 +0200

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,93 @@
-const counter = parent => { return parent.childElementCount };
-const message = elements => {
-    elements.forEach(element => {
-        const firstElem = element.firstElementChild;
-        const secondElem = firstElem.nextElementSibling
-        console.log(`Category: ${firstElem.textContent}`);
-        console.log(`Elements: ${counter(secondElem)}`);
-    });
-};
-const categories = document.querySelector('#categories');
-const subCategories = categories.querySelectorAll('.item');
-console.log(`Number of categories: ${counter(categories)}`);
-message(subCategories);
+// setInterval(timer, 1000);
+
+
+// function timer() {
+// 	console.log('!');
+// }
+
+// timer();
+
+
+
+// setInterval(function() {
+// 	console.log('Hay');
+// }, 3000);
+
+// const mude = [1, 2, 3];
+
+// setTimeout(function() {
+//   for(let item of mude){
+//     if(item === 2){
+//         console.log(item + 10);
+//     }else{
+//         console.log(item + 20);
+//     }
+//   }
+// },5000); 
+
+// let i = 1;
+// setInterval(function(){
+//     i++;
+//     console.log(i);
+// },100);
+
+// let timerId = setInterval(function() {
+// 	console.log('!')
+// }, 1000);
+
+// alert(timerId); // выведет номер таймера
+// let i = 0;
+// let timerId = setInterval(() => {
+//     console.log(i++);
+//     if(i >= 11){
+//        console.log(i--);
+//     }
+    
+// },500);
+
+
+// let i = 0;
+
+// setInterval(function plus () {
+//    console.log(i++);
+// },1000);
+
+// setInterval(function minus (){
+//     console.log(i--);
+// },2000);
+
+
+// if(i<=10) {
+//     plus();
+//     }else {
+//         minus();
+//     }
+
+// let start = document.querySelector('#start');
+// let stop = document.querySelector('#stop');
+
+
+// // start.addEventListener('click', function func(){
+// //     let i = 0;
+
+// //     setInterval(function(){
+// //         console.log(i++);
+// //     },2000);
+// //     this.removeEventListener('click', func);
+// // })
+
+
+
+// let timerId; // сделаем переменную глобальной
+
+// start.addEventListener('click', function() {
+// 	let i = 0;
+	
+// 	timerId = setInterval(function() {
+// 		console.log('i++')
+// 	}, 1000);
+// });
+
+// stop.addEventListener('click', function() {
+// 	clearInterval(timerId);
+// });
--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-
-const input = document.querySelector('#font-size-control'),
-text = document.querySelector('#text');
-
-input.addEventListener('input',() => {
-text.style.fontSize = input.value + 'px';
-})
