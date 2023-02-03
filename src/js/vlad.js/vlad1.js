82a44442f649d5c0b1dab1912b95d4cf55057fdb
Vladyslav Belotskiy <vlad.belotskiy9595@gmail.com>
2023-02-03 11:25:30 +0200

    1

---
+++ src/js/vlad.js/vlad1.js
@@ -0,0 +1,46 @@
+// "use strict";
+// //alert('Привет, Влад!');
+
+// let a = 3;
+// alert(a);
+
+// let num;
+// num = 123;
+
+// alert(num);
+
+// let a;
+
+// a = 1;
+// alert(a);
+
+// a = 2;
+// alert(a);
+// let a = `Приветbr`;
+// alert(a.length);
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// let txt  = 'aaa ${str1} bbb ${str2} ccc`;
+// alert(txt);
+
+// let str = 'xxx';
+// let txt = `aaa ${str} bbb`;
+// alert(txt);
+
+// let str1 = `a` 
+// `b`
+// `c`;
+// alert(str);
+
+// let str = `abc
+// def`;
+// alert(str);
+
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+setInterval(function(){
+    var messageInput = document.getElementById('message');
+    window.alert(messageInput.value);
+}, 4000);
