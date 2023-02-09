e084405da9531a76675fbee1b0c9889bd8d07287
Vladlen009 <95219771+Vladlen009@users.noreply.github.com>
2023-02-09 08:43:23 +0000

    Deploying to gh-pages from @ Vladlen009/training.GoIt.-JS@1534cdda7f933763933a7185e929dd495a1b176c 🚀

--- index.0a835579.js
+++
@@ -1,2 +0,0 @@
-setInterval((function(){var e=document.getElementById("message");window.alert(e.value)}),4e3);
-//# sourceMappingURL=index.0a835579.js.map
---
+++ index.25e8329a.js
@@ -0,0 +1,2 @@
+var obj={x:2,y:2,z:3},key="x";obj[key]>5?alert("Hay"):alert("Job");
+//# sourceMappingURL=index.25e8329a.js.map
