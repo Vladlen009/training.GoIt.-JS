1534cdda7f933763933a7185e929dd495a1b176c
Vladyslav Belotskiy <vlad.belotskiy9595@gmail.com>
2023-02-09 10:42:48 +0200

    Update vlad1.js

--- src/js/vlad.js/vlad1.js
+++ src/js/vlad.js/vlad1.js
@@ -40,7 +40,104 @@
 // const hotelName = prompt("Please enter desired hotel name");
 // console.log(hotelName);
 
-setInterval(function(){
-    var messageInput = document.getElementById('message');
-    window.alert(messageInput.value);
-}, 4000);
+// setInterval(function(){
+//     var messageInput = document.getElementById('message');
+//     window.alert(messageInput.value);
+// }, 4000);
+
+// let a;
+// alert(a)
+
+// let a = null;
+// alert(a);
+
+// console.log(123);
+// console.log('123');
+
+// const a = 'Вла';
+// const b = 'Дик';
+// alert(a+b);
+// alert(a*b);
+
+// let a = 12345;
+// let b = String(a);
+
+// alert(b[b.length -1] + b[3] + b[2] + b[1] + b[0] );
+
+// let num = 47;
+// num ++;
+// // num -= 18;
+// // num *= 10;
+// // num /= 15;
+// alert(num);
+
+// let num1 = prompt('Введите сторону квадрата');
+
+
+// alert('Площадь равна ' +num1**2);
+// let num1 = 1;
+// let num2 = 2;
+// alert(num1 + num2);
+
+// alert((((60 * 60)*24))*30);
+
+// let arr = ['a', 'b', 'c', 'd'];
+
+// let a = arr[0];
+// let b = arr[1];
+// let c = arr[2];
+// let d = arr[3];
+
+// alert(a+b+c+d);
+
+// arr[0] = 1;
+
+// alert(arr);
+
+// arr[0] +=3;
+// arr[1] +=3;
+
+// alert(arr);
+
+// let obj = {a: 1, b: 2, c: 3};
+// console.log(obj['a']);
+
+// let obj = {name: 'vlad', age: 27, surname: 'bel'}
+
+// console.log(obj[age])
+
+// let matvey = 'gorshok';
+
+
+
+// for(let i= 0; i<10; i++){
+//     if(matvey === 'gorshok') {
+//         console.log('Горшок')
+//     } else {
+//         console.log('На пол')
+//     }
+     
+// }
+
+// 'name', 'surname', 'patronymic'
+
+
+// let obj = {name:'Vlad', surname: 'Bel', age: '27'}
+
+// console.log(obj.name, obj.surname, obj.age)
+
+// let keys = Object.keys(obj).length;
+
+// console.log(keys);
+
+let obj = {x: 2, y: 2, z: 3};
+let key = 'x';
+
+// console.log(obj[key]);
+
+if(obj[key] > 5) {
+    alert('Hay')
+}else{
+    alert('Job')
+}
+
